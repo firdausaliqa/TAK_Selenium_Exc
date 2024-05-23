4e23fc211aae6972500455322ef861088c5db661
M Firdaus Ali <firdausaliwork@gmail.com>
2024-05-23 13:30:22 +0700

    update import

--- register.py
+++ register.py
@@ -1,11 +1,11 @@
 import unittest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from pageObject.homePage import regisPage
 from pageObject.loginPage import loginData,  loginPage
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
 
 class DemowebShop(unittest.TestCase):
     
